c06739bb2de5589d03cd36c6e25fc6ca98af05de
George <chaoboma@qq.com>
2024-12-03 16:10:27 +0800

    Feature Using loguru to record and rotate log file,this method is perfect

--- src/main.py
+++ src/main.py
@@ -7,12 +7,12 @@ app.conf.update(
     CELERYBEAT_SCHEDULE={
         "crontab_func1": {
             'task': 'tasks.crontab_func1',
-            'schedule': crontab(minute='*/1'),
+            'schedule': crontab(minute='*/5'),
             'args': ()
         },
         "crontab_func2": {
             'task': 'tasks.crontab_func2',
-            'schedule': crontab(minute='*/1'),
+            'schedule': crontab(minute='*/5'),
             'args': ()
         },
     },
